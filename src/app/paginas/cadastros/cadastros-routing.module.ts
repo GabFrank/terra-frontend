0fee58b298f2dd1c958d60a8cc1726b9259c5be4
Gabriel Franco <francogabrielpy@outlook.com>
2020-02-06 21:56:28 -0300

    mudando de css a scss

--- src/app/paginas/cadastros/cadastros-routing.module.ts
+++ src/app/paginas/cadastros/cadastros-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CadastrosComponent } from './cadastros.component';
 
 
 const routes: Routes = [
+  {path: '', component: CadastrosComponent},
   {path: 'clientes', loadChildren: () => import('./clientes/clientes.module').then(m => m.ClientesModule)}
 ];
 
